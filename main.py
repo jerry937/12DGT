81c353ba63e154dd76062a8fe421293bab9863a7
jerry937 <jerryqian81@gmail.com>
2021-06-03 08:51:39 +1200

    updating fil of website frame

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+import sqlite3
 
+
+app = Flask(__name__)
 @app.route('/')
 def home ():
-    return render_template('home.html',
-    title = ("ool Talo"))
+    return render_template('home.html', title = "ool Talo")
 
 
-import sqlite3
+
+
 @app.route('/pizza/<int:id>')
 def pizza(id):
   conn = sqlite3.connect('pizza.db')
@@ -18,54 +20,8 @@ def pizza(id):
   conn.close()
   return render_template('pizza.html',pizza=pizza)
   
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-@app.route('/')
-def about():
-  return render_template('home.html')
-
-@app.route('/thing/<int:num>')
-def thing (num):
-  num2=num*num 
-  return render_template('thing.html',
-         num=num,num2=num2)
- 
-    
-
 
 if __name__=="__main__":
-     app.run(port=8080,host='0.0.0.0',debug=True)
-     
-     
+     app.run(port=8080,host='127.0.0.0',debug=True)
 
 
-
-
- 
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
