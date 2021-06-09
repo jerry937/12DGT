4732ca08da92a718d120f825103b0e78011dfeba
jerry937 <jerryqian81@gmail.com>
2021-06-09 14:34:31 +1200

    navbar working

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from flask import Flask, render_template
 import sqlite3
-
-
 app = Flask(__name__)
+
+
 @app.route('/')
 def home ():
-    return render_template('home.html', title = "ool Talo")
+    return render_template('home.html',
+    title = ("ool Talo"))
+
+
+@app.route('/all_cake')
+def all_cake ():
+    return render_template('/all_cake.html')
+   
 
 
 
 
-@app.route('/pizza/<int:id>')
-def pizza(id):
-  conn = sqlite3.connect('pizza.db')
 
-  cursor = conn.cursor()
-  cursor.execute("SELECT*FROM pizza WHERE id=?;",(1,))
-  pizza = cursor.fetchone()
-  conn.close()
-  return render_template('pizza.html',pizza=pizza)
-  
 
 if __name__=="__main__":
-     app.run(port=8080,host='127.0.0.0',debug=True)
-
-
+     app.run(port=8080 ,host='127.0.0.1',debug=True)
